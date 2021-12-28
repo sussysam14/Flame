90616f05ef511cbb85436cb31ef1d74f2fd801ef
sussysam14 <96638034+sussysam14@users.noreply.github.com>
2021-12-28 13:47:45 -0500

    Add files via upload

---
+++ Clientdata.py
@@ -0,0 +1,65 @@
+from functools import _Descriptor, total_ordering
+from os import X_OK
+from sys import version
+from types import ModuleType
+from typing import ClassVar, Collection, Reversible, ValuesView, no_type_check_decorator
+replace
+from functools import _Descriptor, total_ordering
+from os import X_OK
+from types import ModuleType
+from typing import ClassVar, Reversible, ValuesView, no_type_check_decorator <-- Replace as = assert Client
+ 
+
+
+(
+    Collection
+    module 
+    str version
+    
+)
+view print = models
+ when selected Reversible <--- Fix errors
+ClassVar str ModuleType  
+  StopAsyncIteration 
+  no_type_check_decorator
+ValuesView (function) no_type_check_decorator: 
+(decorator: Callable[_P@no_type_check_decorator, _T@no_type_check_decorator]) 
+-> Callable[_P@no_type_check_decorator, _T@no_type_check_decorator]
+ finally 
+ open 
+ str pow no_type_check_decorator  
+ finally _Descriptor  ClassVar 
+ set finally = total_ordering
+macOS:.set
+vars: (__object: Any = str model
+    ...) -> dict[str, Any]
+from _py_abc
+and (function) = Enable
+ (X_OK if 
+   total_ordering 
+   
+     str project
+convert)
+view print = models
+ when selected Reversible <--- Fix errors
+ClassVar str ModuleType  
+  StopAsyncIteration 
+  no_type_check_decorator
+ValuesView (function) no_type_check_decorator: 
+(decorator: Callable[_P@no_type_check_decorator, _T@no_type_check_decorator]) 
+-> Callable[_P@no_type_check_decorator, _T@no_type_check_decorator]
+ finally 
+ open 
+ str pow no_type_check_decorator  
+ finally _Descriptor  ClassVar 
+ set finally = total_ordering
+macOS:.set
+vars: (__object: Any = str model
+    ...) -> dict[str, Any]
+from _py_abc
+and (function) = Enable
+ (X_OK if 
+   total_ordering 
+   
+     str project
+convert)
---
+++ exe.py
@@ -0,0 +1,29 @@
+from functools import _Descriptor, total_ordering
+from os import X_OK
+from types import ModuleType
+from typing import ClassVar, Reversible, ValuesView, no_type_check_decorator
+
+
+view print = models
+ when selected Reversible <--- Fix errors
+ClassVar str ModuleType  
+  StopAsyncIteration 
+  no_type_check_decorator
+ValuesView (function) no_type_check_decorator: 
+(decorator: Callable[_P@no_type_check_decorator, _T@no_type_check_decorator]) 
+-> Callable[_P@no_type_check_decorator, _T@no_type_check_decorator]
+ finally 
+ open 
+ str pow no_type_check_decorator  
+ finally _Descriptor  ClassVar 
+ set finally = total_ordering
+macOS:.set
+vars: (__object: Any = str model
+    ...) -> dict[str, Any]
+from _py_abc
+and (function) = Enable
+ (X_OK if 
+   total_ordering 
+   
+     str project
+convert)
